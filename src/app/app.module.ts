85bcf277fa88ffd9d27210d33e718d56a4cca7cc
Aman Aggarwal <guptaman49@gmail.com>
2022-11-26 20:47:48 +0530

    new comp generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HeroComponent
+    HeroComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
